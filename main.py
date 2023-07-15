bd6bdfa4f110d95b3ef381bf6c762b6221eef9ea
gregs0n <volkovpy@mpei.ru>
2023-07-15 22:28:17 +0300

    Add echo_bot template from kmsint

---
+++ main.py
@@ -0,0 +1,42 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+from config.config import Config, load_config
+from handlers import other_handlers, user_handlers
+
+cnfg: Config = load_config('.env')
+
+bot: Bot = Bot(token=cnfg.tg_bot.token)
+dp: Dispatcher = Dispatcher()
+
+
+# Функция конфигурирования и запуска бота
+async def main() -> None:
+
+    # Загружаем конфиг в переменную config
+    config: Config = load_config()
+
+    # Инициализируем бот и диспетчер
+    bot: Bot = Bot(token=config.tg_bot.token)
+    dp: Dispatcher = Dispatcher()
+
+    # Регистриуем роутеры в диспетчере
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
+
+    # Пропускаем накопившиеся апдейты и запускаем polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO,
+                        filename="updates.log", 
+                        encoding='utf-8',
+                        format='%(asctime)s %(message)s')
+    #logging.info(msg)
+    asyncio.run(main())
