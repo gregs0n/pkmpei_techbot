dbd12021c15b247f5127cc3010c064f7e9bec72c
gregs0n <volkovpy@mpei.ru>
2023-07-17 15:21:31 +0300

    Change config processing

--- config/config.py
+++ config/config.py
@@ -1,6 +1,6 @@
+# - *- coding: utf- 8 - *-
+import configparser
 from dataclasses import dataclass
-from environs import Env
-
 
 @dataclass
 class TgBot:
@@ -10,9 +10,33 @@ class TgBot:
 @dataclass
 class Config:
     tg_bot: TgBot
+    RATE_LIMIT: float
+    PATH_DATABASE: str
+    PATH_LOGS: str
+    ADMINS: list[int]
 
 
 def load_config(path: str | None = None) -> Config:
-    env = Env()
-    env.read_env(path)
-    return Config(tg_bot=TgBot(token=env('BOT_TOKEN')))
+    read_config = configparser.ConfigParser()
+    read_config.read('settings.ini')
+
+    BOT_TOKEN = read_config['settings']['token'].strip()  # Токен бота
+    RATE_LIMIT = float(read_config['settings']['rate_limit'].strip())  # Антифлуд
+    PATH_DATABASE = 'data/database.db'  # Путь к БД
+    PATH_LOGS = 'data/logs.log'  # Путь к Логам
+    
+    admins = read_config['settings']['admin_id'].strip()
+    admins = admins.replace(' ', '')
+
+    admins = admins.split(',')
+    admins = list(map(int, admins))
+    
+    cnf = Config(
+        tg_bot=TgBot(token=TgBot(token=BOT_TOKEN)),
+        RATE_LIMIT=RATE_LIMIT,
+        PATH_DATABASE=PATH_DATABASE,
+        PATH_LOGS=PATH_LOGS,
+        ADMINS=admins
+    )
+    
+    return cnf
