b504a610de4b52ad5e848737ef805891a29dbb56
gregs0n <volkovpy@mpei.ru>
2023-07-27 11:44:33 +0300

    Add InlineKeyBoardBuilder

---
+++ keyboards/keyboards.py
@@ -0,0 +1,19 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+
+# Функция для формирования инлайн-клавиатуры на лету
+def create_inline_kb(width: int,
+                     **kwargs: str) -> InlineKeyboardMarkup:
+    kb_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
+    buttons: list[InlineKeyboardButton] = []
+
+    if kwargs:
+        for button, text in kwargs.items():
+            buttons.append(InlineKeyboardButton(
+                text=text,
+                callback_data=button))
+
+    kb_builder.row(*buttons, width=width)
+
+    return kb_builder.as_markup()
--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import logging
 
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.types import BotCommand
 
 from config.config import Config, load_config
 from handlers.admin_handlers import admin_router
 from handlers.user_handlers import user_router
+from handlers.other_handlers import last_router
 from keyboards.set_menu import set_main_menu
 
 async def main() -> None:
@@ -27,6 +27,7 @@ async def main() -> None:
     dp.startup.register(set_main_menu)
     dp.include_router(user_router)
     dp.include_router(admin_router)
+    dp.include_router(last_router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
